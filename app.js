064bbddd8e811b67051ab525a4d42c7933b2d466
G1edriusP <giedrius.pakalniskis@gmail.com>
2021-10-02 14:04:41 +0300

    Init project

---
+++ app.js
@@ -0,0 +1,46 @@
+import express, { json } from "express";
+
+// Mock data
+import { ADVERTS } from "./data/Advert.js";
+
+const app = express();
+app.use(json());
+
+// -----
+// GET adverts
+app.get("/adverts", (req, res, next) => {
+  res.status(200).send(ADVERTS);
+});
+
+// GET advert
+app.get("/adverts/:id", (req, res, next) => {
+  const id = JSON.parse(req.params.id);
+  const advert = ADVERTS.find((ad) => ad.id === id);
+  res.status(200).send(advert);
+});
+
+// POST advert
+app.post("/adverts", (req, res, next) => {
+  const data = req.body;
+  res.status(201).send(data);
+});
+
+// PATCH advert
+app.patch("/adverts/:id", (req, res, next) => {
+  const id = req.params.id;
+  const data = req.body;
+  const advert = ADVERTS.find((ad) => ad.id === id);
+  const updatedAdvert = { ...advert, ...data };
+  res.status(200).send(updatedAdvert);
+});
+
+// DELETE advert
+// app.post("/adverts/:id", (req, res, next) => {
+//   const data = req.body;
+//   res.status(201).send(data);
+// });
+// -----
+
+app.listen(8080, () => {
+  console.log(`Server is running`);
+});
---
+++ data/Advert.js
@@ -0,0 +1,94 @@
+export const ADVERTS = [
+  {
+    id: 1,
+    title: "Skelbimas #1",
+    organization: "Organization1",
+    location: {
+      address: "Kaunas",
+      coords: {
+        longitude: "34.5677",
+        latitude: "45.6767",
+      },
+    },
+    info: {
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tempor, dui sed ullamcorper scelerisque, mi justo vehicula nibh, in venenatis purus purus sed mauris. Quisque feugiat mi nibh.",
+      duration: {
+        years: 0,
+        months: 0,
+        days: 0,
+      },
+      applicationsCount: 0,
+      web: "https://www.google.com",
+    },
+  },
+  {
+    id: 2,
+    title: "Skelbimas #2",
+    organization: "Organization2",
+    location: {
+      address: "Vilnius",
+      coords: {
+        longitude: "34.5677",
+        latitude: "45.6767",
+      },
+    },
+    info: {
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tempor, dui sed ullamcorper scelerisque, mi justo vehicula nibh, in venenatis purus purus sed mauris. Quisque feugiat mi nibh.",
+      duration: {
+        years: 0,
+        months: 0,
+        days: 0,
+      },
+      applicationsCount: 0,
+      web: "https://www.google.com",
+    },
+  },
+  {
+    id: 3,
+    title: "Skelbimas #3",
+    organization: "Organization1",
+    location: {
+      address: "Kaunas",
+      coords: {
+        longitude: "34.5677",
+        latitude: "45.6767",
+      },
+    },
+    info: {
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tempor, dui sed ullamcorper scelerisque, mi justo vehicula nibh, in venenatis purus purus sed mauris. Quisque feugiat mi nibh.",
+      duration: {
+        years: 0,
+        months: 0,
+        days: 0,
+      },
+      applicationsCount: 0,
+      web: "https://www.google.com",
+    },
+  },
+  {
+    id: 4,
+    title: "Skelbimas #4",
+    organization: "Organization3",
+    location: {
+      address: "Kaunas",
+      coords: {
+        longitude: "34.2077",
+        latitude: "50.6767",
+      },
+    },
+    info: {
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tempor, dui sed ullamcorper scelerisque, mi justo vehicula nibh, in venenatis purus purus sed mauris. Quisque feugiat mi nibh.",
+      duration: {
+        years: 0,
+        months: 0,
+        days: 0,
+      },
+      applicationsCount: 0,
+      web: "https://www.google.com",
+    },
+  },
+];
